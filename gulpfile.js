7daa23c083ae5c2a857864ae77efc749b3786c19
ligeng846153442 <846153442@qq.com>
2018-10-12 22:14:24 +0800

    2018/10/12

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,25 @@ var gulp = require("gulp");//本地安装gulp所用到地方
 //复制HTML文件
 gulp.task("copyhtml",function(){
 	gulp.src("index.html")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin1"));
 });
 //复制img文件
 gulp.task("copyimg",function(){
 	gulp.src("img/**/*")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin\\img"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin1\\img"));
 });
 //复制css文件
 gulp.task("copycss",function(){
 	gulp.src("css/**/*")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin\\css"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin1\\css"));
 });
 //复制js文件
 gulp.task("copyjs",function(){
 	gulp.src("js/**/*")
-	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin\\js"));
+	.pipe(gulp.dest("D:\\phpStudy\\WWW\\aiqianjin1\\js"));
 });
 
-gulp.task("build",["copyhtml","copycss","copyjs","copyimg",]);
+gulp.task("bulid",["copyhtml","copycss","copyjs","copyimg",]);
 
 gulp.task("watchall",function(){
 	gulp.watch("*.html",["copyhtml"]);
